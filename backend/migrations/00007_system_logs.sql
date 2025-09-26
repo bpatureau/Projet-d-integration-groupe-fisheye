e8ac8cf1579417257db63a8725a446d18986a556
Simon Fontaine <github@simonfontaine.com>
2025-09-26 09:56:51 +0200

    fix(backend): added postgresql pgcrypto extension

---
+++ backend/migrations/00007_system_logs.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS system_logs (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  level VARCHAR(20) NOT NULL CHECK (level IN ('info', 'warning', 'error')),
+  message TEXT NOT NULL,
+  component VARCHAR(50),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE system_logs;
+-- +goose StatementEnd
